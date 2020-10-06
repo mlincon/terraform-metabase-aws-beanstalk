bde5da1f5829031c13f95f75884dc9eb9ac1418e
mlincon <mehdi.lincon@gmail.com>
2020-10-07 00:22:59 +0200

    output the IAM role ARN

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "ebs_service_role" {
+  description = "Role ARN"
+  value = aws_iam_role.ebs-service-role.arn
+}
+
 output "metabase_endpoint" {
   description = "Application Endpoint"
   value       = aws_elastic_beanstalk_environment.metabase-env.cname
